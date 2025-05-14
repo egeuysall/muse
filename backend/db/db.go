88b899c020855458e83fd4227e0e78d111ed85b7
Ege Uysal <hello@egeuysal.com>
2025-05-13 22:49:07 -0500

    Modify tests

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,3 +1,9 @@
 package db
 
-// Purpose: Connects to your Postgres database.
+import "github.com/nedpals/supabase-go"
+
+var Client *supabase.Client
+
+func InitSupabase(url, key string) {
+	Client = supabase.CreateClient(url, key)
+}.
